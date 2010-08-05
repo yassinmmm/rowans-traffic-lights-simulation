29afeb34b7215c4dbf177442c51582390dac02a7
rowan.fletcher@gmail.com <rowan.fletcher@gmail.com@5b3a8bbf-1318-c304-d91e-1727076b083a>
2010-08-05 12:38:14 +0000

    Fixed some errors in menu code. Optimized input validity-checking. Implemented menu options 3 & 4.

--- TrafficLights/src/trafficlights/Main.java
+++ TrafficLights/src/trafficlights/Main.java
@@ -12,16 +12,22 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static int h_street = 1;
+    private static double h_prob;
+    private static int v_street = 1;
+    private static double v_prob;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int ans;
-        int h_street = 1;
-        int v_street = 1;
 
         while (true) {
+            String input = null;
+            int number = -123456789;
+            double prob;
+
             System.out.print("=== MENU ====\n"
                     + "1. Set Number of horizontal lanes (H-street) [min 1, max 3]\n"
                     + "2. Set Number of vertical lanes (V-street) [min 1, max 4]\n"
@@ -30,30 +36,41 @@ public class Main {
                     + "5. Run one simulation cycle\n"
                     + "6. Set and run number of simulation cycles [min 1, max 10]\n\n"
                     + "Enter your choice> ");
-            ans = scanner.nextInt();
-            ///System.out.println(ans);
-            ///v_street = System.in.read();
-            //System.out.println(v_street);
-            switch (ans) {
+
+            //If user enters ans value as input then program
+            //produces errors
+            while (number == -123456789) {
+                input = scanner.next();
+                try {
+                    number = Integer.parseInt(input);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Please enter a valid digit!");
+                    System.out.print("Enter your choice> ");
+                }
+            }
+
+
+            switch (number) {
                 case 1:
-                    int H_number;
-                    String H_val = "";
+                    //If user enters H_number value as input then program
+                    //produces errors
+                    number = -123456789;
 
                     System.out.print("Number of horizontal lanes [min 1, max 3]: ");
 
                     while (true) {
-                        H_number = 0;
-                        while (H_number == 0) {
-                            H_val = scanner.next();
+                        number = -123456789;
+                        while (number == -123456789) {
+                            input = scanner.next();
                             try {
-                                H_number = Integer.parseInt(H_val);
+                                number = Integer.parseInt(input);
                             } catch (NumberFormatException nfe) {
                                 System.out.println("Please enter a digit!");
                                 System.out.print("Number of horizontal lanes [min 1, max 3]: ");
                             }
                         }
 
-                        if (H_number < 1 || H_number > 3) {
+                        if (number < 1 || number > 3) {
                             System.out.println("There must be more than 1 lane and less than 3!");
                             System.out.print("Number of horizontal lanes [min 1, max 3]: ");
                         } else {
@@ -61,46 +78,103 @@ public class Main {
                         }
                     }
 
-                    h_street = H_number;
-                    System.out.println("The simulation now has " + h_street + " horizontal lanes.");
-
+                    h_street = number;
+                    System.out.println("The simulation now has " + h_street + " horizontal lanes.\n");
                     break;
-                case 2:
-                    int V_number;
-                    String V_val = "";
 
-                    System.out.print("Number of horizontal lanes [min 1, max 4]: ");
+                case 2:
+                    //If user enters V_number value as input then program
+                    //produces errors
+                    number = -123456789;
+
+                    System.out.print("Number of vertical lanes [min 1, max 4]: ");
 
                     while (true) {
-                        V_number = 0;
-                        while (V_number == 0) {
-                            V_val = scanner.next();
+                        number = -123456789;
+                        while (number == -123456789) {
+                            input = scanner.next();
                             try {
-                                V_number = Integer.parseInt(V_val);
+                                number = Integer.parseInt(input);
                             } catch (NumberFormatException nfe) {
                                 System.out.println("Please enter a digit!");
-                                System.out.print("Number of horizontal lanes [min 1, max 4]: ");
+                                System.out.print("Number of vertical lanes [min 1, max 4]: ");
                             }
                         }
 
-                        if (V_number < 1 || V_number > 4) {
+                        if (number < 1 || number > 4) {
                             System.out.println("There must be more than 1 lane and less than 4!");
-                            System.out.print("Number of horizontal lanes [min 1, max 4]: ");
+                            System.out.print("Number of vertical lanes [min 1, max 4]: ");
                         } else {
                             break;
                         }
                     }
 
-                    v_street = V_number;
-                    System.out.println("The simulation now has " + v_street + " vertical lanes.");
+                    v_street = number;
+                    System.out.println("The simulation now has " + v_street + " vertical lanes.\n");
+                    break;
 
-                    break;
                 case 3:
-                    System.out.println("Three");
+                    //If user enters prob value as input then program
+                    //produces errors
+                    prob = -0.123456789;
+
+                    System.out.print("Probability of cars entering horizontal lane [min 0.0, max 1.0]: ");
+
+                    while (true) {
+                        prob = -0.123456789;
+                        while (prob == -0.123456789) {
+                            input = scanner.next();
+                            try {
+                                prob = Double.parseDouble(input);
+                            } catch (NumberFormatException nfe) {
+                                System.out.println("Please enter a digit!");
+                                System.out.print("Probability of cars entering horizontal lane [min 0.0, max 1.0]: ");
+                            }
+                        }
+
+                        if (prob < 0 || prob > 1) {
+                            System.out.println("Probability must be between 0.0 & 1.0!");
+                            System.out.print("Probability of cars entering horizontal lane [min 0.0, max 1.0]: ");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    v_prob = prob;
+                    System.out.println("Cars now have a " + v_prob + " chance of entering the horizontal lanes\n");
                     break;
+
                 case 4:
-                    System.out.println("Four");
+                    //If user enters prob value as input then program
+                    //produces errors
+                    prob = -0.123456789;
+
+                    System.out.print("Probability of cars entering vertical lane [min 0.0, max 1.0]: ");
+
+                    while (true) {
+                        prob = -0.123456789;
+                        while (prob == -0.123456789) {
+                            input = scanner.next();
+                            try {
+                                prob = Double.parseDouble(input);
+                            } catch (NumberFormatException nfe) {
+                                System.out.println("Please enter a digit!");
+                                System.out.print("Probability of cars entering vertical lane [min 0.0, max 1.0]: ");
+                            }
+                        }
+
+                        if (prob < 0 || prob > 1) {
+                            System.out.println("Probability must be between 0.0 & 1.0!");
+                            System.out.print("Probability of cars entering vertical lane [min 0.0, max 1.0]: ");
+                        } else {
+                            break;
+                        }
+                    }
+
+                    v_prob = prob;
+                    System.out.println("Cars now have a " + v_prob + " chance of entering the vertical lanes\n");
                     break;
+
                 case 5:
                     System.out.println("Five");
                     break;
