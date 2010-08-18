5c5c52f96a14fa9e85a580b59cf1e82ffc9fdd69
rowan.fletcher@gmail.com <rowan.fletcher@gmail.com@5b3a8bbf-1318-c304-d91e-1727076b083a>
2010-08-18 05:04:29 +0000

    Resolved tree conflict state of Main.
    Code is more OO standardised.
    Cleaner implementation of draw method

---
+++ TrafficLights/src/trafficlights/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trafficlights;
+
+/**
+ *
+ * @author Confucius
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        while (true) {
+            try {
+               Menu.Menu();
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter a valid digit!");
+            }
+        }
+    }
+}
